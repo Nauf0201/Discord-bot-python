0b32ee7f86d886a080986587d9b5503190434e5c
Nauf0201 <132118774+Nauf0201@users.noreply.github.com>
2023-05-27 12:29:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -15,11 +15,20 @@ async def hello(ctx):
     await ctx.send(f'Hi! I am a bot {bot.user}!')
 
 @bot.command()
-async def senyum(ctx):
+async def smile(ctx):
     await ctx.send(f':D')
+
+@bot.command()
+async def plus(ctx, letter_1, letter_2):
+    await ctx.send(int(letter_1) + int(letter_2))
+
+@bot.command()
+async def multiply(ctx, number_1, number_2):
+    await ctx.send(int(number_1) * int(number_2))
+
 
 @bot.command()
 async def heh(ctx, count_heh = 5):
     await ctx.send("he" * count_heh)
 
-bot.run("SECRET TOKEN")
+bot.run("YOUR TOKEN HERE!")
