1713c4c8f23ccbbcb8c2304ab679eb10a4ed9c5b
Nauf0201 <132118774+Nauf0201@users.noreply.github.com>
2023-06-10 12:13:28 +0700

    Update bot_1.py

--- bot_1.py
+++ bot_1.py
@@ -7,13 +7,27 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='$', intents=intents)
 
+#List of trash can
+
+trash_list_o = ['Leaf', 'fruit', 'Dirt']
+
+trash_list_a = ['Plastic', 'Bottle', 'Tissue']
+
+trash_list_b = ['Battery', 'Lamp', 'Drug']
+
+#on command
+
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
+#Hello
+
 @bot.command()
 async def hello(ctx):
     await ctx.send(f'Hi! I am a bot {bot.user}!')
+
+#emotion
 
 @bot.command()
 async def smile(ctx):
@@ -22,6 +36,8 @@ async def smile(ctx):
 @bot.command()
 async def cry(ctx):
     await ctx.send(f':_(')
+
+#Calculator
 
 @bot.command()
 async def plus(ctx, letter_1, letter_2):
@@ -35,8 +51,25 @@ async def multiply(ctx, number_1, number_2):
 async def rank_number(ctx, number_1, number_2):
     await ctx.send(int(number_1) ** int(number_2))
 
+#Trash can
+
+@bot.command()
+async def trash(ctx):
+    await ctx.send('Enter the trash name')
+    msg = await bot.wait_for("message")
+    if msg.content in trash_list_a:
+        await ctx.send('Put it in the inorganic trash')
+    elif msg.content in trash_list_o:
+        await ctx.send('Put it in the organic trash')
+    elif msg.content in trash_list_b:
+        await ctx.send('Put it in the dangerous trash can')
+
+#hehehe
+
 @bot.command()
 async def heh(ctx, count_heh = 5):
     await ctx.send("he" * count_heh)
 
-bot.run("SECRET TOKEN HERE")
+#Token
+
+bot.run("SECRET TOKEN")
